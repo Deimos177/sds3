cca3d4d49829e211e7b8c6731c11da0215c1b2e0
Deimos177 <brucevieira.2009@gmail.com>
2021-05-05 10:09:49 -0300

    Initial commit

---
+++ backend/sds3/src/main/java/com/br/sds3/Sds3Application.java
@@ -0,0 +1,13 @@
+package com.br.sds3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sds3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sds3Application.class, args);
+	}
+
+}
